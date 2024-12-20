2a33d182286c7b54d8ef189a3b6c79f2f325549d
ilay <ilay8073@gmail.com>
2024-12-20 21:23:15 +0200

    added method and started working on dance.py game

--- dance.py
+++ dance.py
@@ -1,20 +1,44 @@
 import pygame
 from PIL import Image
 
-class dancer:
-    def __init__(self,gender):
-        if gender==1:
+class Dancer(pygame.sprite.Sprite):
+    def __init__(self,gender="M",x=0,y=0,speed=5):
+        super().__init__()
+        if gender.equals("w") or gender.equals("W"):
             PLAYER_IMG = 'female_dancer.png'
-            player_img = pygame.image.load(PLAYER_IMG)
+            self.player_img = pygame.image.load(PLAYER_IMG).convert_alpha()
 
         else:
             PLAYER_IMG = 'male_dancer.png'
-            player_img = pygame.image.load(PLAYER_IMG)
-            player_img = pygame.transform.flip(player_img, True, False)  # היפוך התמונה מימין לשמאל
-        player_img = pygame.transform.scale(player_img, (85, 110))  # הקטנה גודל האווטאר
+            self.player_img = pygame.image.load(PLAYER_IMG).convert_alpha()
+            self.player_img = pygame.transform.flip(self.player_img, True, False)  # היפוך התמונה מימין לשמאל
         avatar_image = Image.open(PLAYER_IMG)  # פתיחת הקובץ באמצעות PIL כדי לקרוא צבע פיקסל
-        background_ava = avatar_image.getpixel((0, 0))  # דגימת צבע הפיקסל מהפינה השמאלית העליונה
-        player_img.set_colorkey(background_ava)  # הפיכת צבע הרקע לתמונה שקוף
+        self.player_img.set_colorkey(avatar_image.getpixel((0, 0)))  # הפיכת צבע הרקע לתמונה שקוף
+        self.rect = self.player_img.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+        self.direction = pygame.math.Vector2()
+    def update(self):
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
+
+    def scale(self, width=85, height=110):
+        self.player_img = pygame.transform.scale(self.player_img, (width, height))
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def rotate(self, angle):
+        self.player_img = pygame.transform.rotate(self.player_img, angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
 
+WIDTH = 800
+HEIGHT = 600
 
+
+pygame.init()
+pygame.mixer.init()
+pygame.mouse.set_visible(True)  # הצגת סמן העכבר
+size = (WIDTH, HEIGHT)  # קביעת גודל המסך
+screen = pygame.display.set_mode(size)  # יצירת המסך
+pygame.display.set_caption("Mouse Avatar")  # כותרת חלון המשחק
